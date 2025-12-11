63352065e778fba0c66f956087fb56322c81ad47
Sandra0003 <sandrapuramthottathil54@gmail.com>
2025-12-11 11:16:38 +0530

    commit two

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
+print("*")
+print("**")
+print("***")
+print("***")
+print("**")
+print("*")
 
+
+print("   /|")
+print("  / |")
+print(" /  |")
+print("/   |")
+
+chra_name = "sam"
+print(chra_name)
+isMale = False
+print(isMale)
+
+print("\n Sandra \n and\n savio  \n are \ sibilings")
+
+name = "savio"
+print(name.upper().isupper())
