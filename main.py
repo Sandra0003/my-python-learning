b058e8f74b7f3fac92020a113c6533029833edd3
Sandra0003 <sandrapuramthottathil54@gmail.com>
2025-12-15 10:42:43 +0530

    calculator learn

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from math import *
+
+from orca.debug import eventDebugLevel
 
 print("*")
 print("**")
@@ -70,3 +73,52 @@ print(int(g))
 fruits = ["apple","orange","banana"]
 x,y ,m= fruits
 print(x,y,m)
+
+
+phrase = "sara is bob"
+print(phrase.index("b"))
+print(phrase.replace("bob","sab"))
+
+for letter in phrase:
+    print(letter)
+
+print(3 * 3)
+print(3 * 3 + 3)
+print(10 / 3)
+print(abs(-10))
+print(abs(10))
+print(pow(2,2))
+print(max(2,6))
+print(min(2,6))
+print(round(2.8))
+print(sqrt(25))
+print(floor(8.9))
+
+x =(input("enter your name"))
+b = int(input("enter your age"))
+print("hai , " + x + " age" , b)
+
+# basic calculater
+def calculater(num1, num2, opr):
+    if opr == "+":
+        return  num1 + num2
+    elif opr == "-":
+        return num1 -num2
+    elif opr == "*":
+        return num1 * num2
+    else:
+        return "enter any opr"
+
+while True:
+    num1 = int(input("Enter number one:"))
+    num2 = int(input("enter  number two: "))
+    opr = input("enter operation :")
+    result = calculater(num1, num2, opr)
+    print(result)
+    cont = input("Do you want to continue?(y/n)")
+    if cont == "n":
+        break
+
+
+
+
